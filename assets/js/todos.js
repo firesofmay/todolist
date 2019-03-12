907366f39f76488303f0289bc61c7545a4bea11c
Mayank Jain <firesofmay@gmail.com>
2019-03-12 11:53:35 +0530

    Add resetFunctionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,35 @@
+var defaultTodos = '<li><span><i class="fa fa-trash"></i></span> Buy Vegetables</li> <li><span><i class="fa fa-trash"></i></span> Go for Salsa Class</li> <li><span><i class="fa fa-trash"></i></span> Plan my holiday</li>';
+var listItemsKey = 'listItems';
+
+function getTodo(){
+    return localStorage.getItem(listItemsKey);
+}
+
+function setTodo(){  
+    localStorage.setItem(listItemsKey, $('ul').html());
+}
+
+function updateTodo(val){  
+    localStorage.setItem(listItemsKey, val);
+}
+
+function resetTodos() {  
+    localStorage.removeItem(listItemsKey);
+    $('ul').html(defaultTodos);
+}
+
+
 // This says attach to ul (which is available at the load of the page)
 // and fire on any li, which even if added dynamically will get fired!
 $('ul').on("click", "li", function(){
     $(this).toggleClass("completed");
-    localStorage.setItem('listItems', $('ul').html());
+    setTodo();
 });
 
 $('ul').on("click", "span", function(event){
     $(this).fadeOut(200, function(){
         $(this).parent().remove();
-        localStorage.setItem('listItems', $('ul').html());
+        setTodo();
     });
     event.stopPropagation();
 });
@@ -18,7 +39,7 @@ $("input[type='text'").on("keypress", function(event){
     if (event.which === 13 && txt != ""){
         $('ul').append("<li><span><i class='fa fa-trash'></i> </span>" + txt + "</li>");
         $(this).val("");
-        localStorage.setItem('listItems', $('ul').html());
+        setTodo();
     }
 })
 
@@ -26,12 +47,18 @@ $("#toggle-form").click(function(){
 	$("input[type='text']").fadeToggle();
 });
 
+$("#reset").click(function(){
+    resetTodos();
+    $("input[type='text'").val("");
+});
+
+
 $(document).ready(function () {
-      todos = localStorage.getItem('listItems');
+      todos = getTodo();
       console.log(todos);
       
       if (todos == null){
-          todos = '<li><span><i class="fa fa-trash"></i></span> Buy Vegetables</li> <li><span><i class="fa fa-trash"></i></span> Go for Salsa Class</li> <li><span><i class="fa fa-trash"></i></span> Plan my holiday</li>';
+          todos = defaultTodos;
       }
       $('ul').html(todos);
 });
